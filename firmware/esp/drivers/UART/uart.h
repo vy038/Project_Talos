ce895ed2ca2ed76ef1d0f8d77c6e2c54afe15f2d
vy038 <victoryu038@gmail.com>
2026-01-06 20:36:55 -0500

    add: UART driver

--- firmware/esp/drivers/UART/uart.h
+++ firmware/esp/drivers/UART/uart.h
@@ -2,10 +2,10 @@
 #define UART_H
 
 #include "esp_err.h"
-#include "driver/uart.h"
 #include <stdint.h>
 
-#define BAUD_RATE       115000
+#define UART_PIN_TX       GPIO_NUM_1
+#define UART_PIN_RX       GPIO_NUM_3
 
 /**
  * @brief Initialize UART
@@ -17,12 +17,24 @@
 esp_err_t xUARTInit(void);
 
 /**
- * @brief Read a byte from UART
+ * @brief Write a byte with UART
  *
- * Read a byte of data from the UART line, writing to a data byte
- * @param data      The data to write to
+ * Write a byte of data on the UART line
+ * @param data The data to write
+ * @param len Length of data to write
  * @return esp_err_t ESP_OK on success, error code on failure
  */
-esp_err_t xUARTRead(uint8_t data);
+esp_err_t xUARTWrite(const uint8_t *data, size_t len);
+
+/**
+ * @brief Read a byte from UART
+ *
+ * Read a byte of data from the UART line, writing to a data byte variable
+ * @param data The data variable to write to
+ * @param max_len Maximum number of bytes to read and allocate
+ * @param bytes_read Actual number of bytes read
+ * @return esp_err_t ESP_OK on success, error code on failure
+ */
+esp_err_t xUARTRead(uint8_t *data, size_t max_len, size_t *bytes_read);
 
 #endif
